d1a261f2b033c0a12b322f81e91fbbf31d564136
Andre Pandiangan <andreagrifa@outlook.com>
2020-05-24 03:26:39 +0700

    update readme.md

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://pricemonitorandre.herokuapp.com/';
-//$config['base_url'] = 'http://localhost/fabelio_test';
+//$config['base_url'] = 'https://pricemonitorandre.herokuapp.com/';
+$config['base_url'] = 'http://localhost/fabelio_test';
 
 /*
 |--------------------------------------------------------------------------
